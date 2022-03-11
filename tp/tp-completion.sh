73d4835b8bea66aa2f87bf7102112592a00852f0
Logan MacDougall <50811887+loganmacdougall@users.noreply.github.com>
2022-03-11 12:45:38 -0400

    Updated the tp files
    Added the options for custom Lab prefix names, and removed the requirement for a username but grabs the username from their bash instead

--- tp/tp-completion.sh
+++ tp/tp-completion.sh
@@ -6,9 +6,11 @@
 # This is used along side the tp script so clicking
 # the tab key will provide auto-completion
 
+USERNAME=$(whoami)
+
 # These variables must be set same as they are in the tp file
-USERNAME="macdougall"
 LABSFOLDER="/users/cs/$USERNAME/Courses/CSCI2122/Labs"
+LABPREFIX="Lab"
 
 _tp_completions() {
     if [ ${#COMP_WORDS[@]} == 2 ]; then
@@ -18,9 +20,9 @@ _tp_completions() {
         case ${COMP_WORDS[1]} in
             "lab")
                 if [ ${#COMP_WORDS[@]} == 3 ]; then
-                    COMPREPLY=($(compgen -W "$(ls ${LABSFOLDER} | grep -e "^\(Lab\)" | cut -c 4- | sed -e ':a;N;$!ba;s/\n/ /g')" "${COMP_WORDS[2]}"))
-                elif [ ${#COMP_WORDS[@]} == 4 -a -d $LABSFOLDER/Lab${COMP_WORDS[2]}/$USERNAME/Lab${COMP_WORDS[2]} ]; then
-                    COMPREPLY=($(compgen -W "$(find $LABSFOLDER/Lab${COMP_WORDS[2]}/$USERNAME/Lab${COMP_WORDS[2]} -maxdepth 1 -mindepth 1 -type d | rev | cut -d "/" -f 1 | rev)" "${COMP_WORDS[3]}"))
+                    COMPREPLY=($(compgen -W "$(ls ${LABSFOLDER} | grep -e "^\($LABPREFIX\)" | cut -c $(expr ${#LABPREFIX} + 1)- | sed -e ':a;N;$!ba;s/\n/ /g')" "${COMP_WORDS[2]}"))
+                elif [ ${#COMP_WORDS[@]} == 4 -a -d $LABSFOLDER/Lab${COMP_WORDS[2]}/$USERNAME/$LABPREFIX${COMP_WORDS[2]} ]; then
+                    COMPREPLY=($(compgen -W "$(find $LABSFOLDER/Lab${COMP_WORDS[2]}/$USERNAME/$LABPREFIX${COMP_WORDS[2]} -maxdepth 1 -mindepth 1 -type d | rev | cut -d "/" -f 1 | rev)" "${COMP_WORDS[3]}"))
                 fi
                 ;;
             *)
